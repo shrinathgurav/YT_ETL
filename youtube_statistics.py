d11722bbbab449a96a9bde67c5bd447f5cd9b553
shrinathgurav <shrinathgurav@gmail.com>
2025-12-15 15:29:26 +0530

    Added .env file with API key hardcoded. Added .env file entry into git ignore file. created function to call Youtube API and get the playListID from specified channel handle.

---
+++ youtube_statistics.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+API_KEY=os.getenv("API_KEY")
+HANDLE="MrBeast"
+url= f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={HANDLE}&key={API_KEY}"
+
+def get_playlist_id():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        # print(json.dumps(data,indent=4))
+
+        channel_items=data["items"][0]
+        channel_playListId = channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playListId)
+        return channel_playListId
+
+    except requests.exceptions.RequestException as reqErr :
+        return reqErr
+
+
+if __name__=="__main__":
+    get_playlist_id()
